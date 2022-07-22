484157456f2d5cc82f4f86e05f57c3547e157381
cccccjt <96898429+cccccjt@users.noreply.github.com>
2022-07-22 12:09:33 +0800

    Add files via upload

---
+++ DBS-2207A-main/app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+from flask import Flask, request, render_template
+
+
+# In[27]:
+
+
+import joblib
+
+
+# In[28]:
+
+
+app = Flask(__name__)
+
+
+# In[29]:
+
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
+        model1 = joblib.load("regression")
+        r1 = model1.predict([[rates]])
+        model2 = joblib.load("tree")
+        r2 = model2.predict([[rates]])
+        return(render_template("index.html", result1 = r1, result2 = r2))
+    else:
+        return(render_template("index.html", result1 = "WAITING", result2 = "WAITING"))
+
+
+# In[30]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
